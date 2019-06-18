fd92db9b728e660c378fcf2561ad4e11546f4289
Stijn Schack <stijn.schack@gmail.com>
2019-06-18 17:37:19 +0200

    added premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,21 +2,75 @@ workspace "Element"
 	configurations { "Debug", "Release" }
 	staticruntime "On"
 	startproject "Element"
-
+	platforms   { "x86", "x64" }	
+	outputdir = "bin/%{cfg.system}-%{cfg.architecture}-%{cfg.buildcfg}/"
+	
+	filter "platforms:x86"
+		architecture "x86"
+	filter "platforms:x64"
+		architecture "x64"
+	
 	filter "system:windows"
-		platforms   { "x86", "x64" }
 		flags {"MultiProcessorCompile" }
 	filter "configurations:Debug"
-		Symbols "On"
+		symbols "On"
 
 	project "Element"
+		location "Element"
+		language "C++"
+		cppdialect "C++17"
 		warnings "Extra"
+		
+		includedirs{
+		"Element/src/",
+		"Element/src/pch/",
+		"thirdparty/glad/include/"
+		}
+		
+		files
+		{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+		}
+		
+		links 
+		{ 
+		"GLFW",
+		"Glad",
+		"opengl32.lib"
+		}
+		pchheader "pch.h"
+		pchsource "src/pch/pch.cpp"
 
 		filter "configurations:Debug"
+			kind("ConsoleApp")
 			defines     "ELM_DEBUG"
-			flags       { "Symbols" }
 
 		filter "configurations:Release"
-			optimize    "Full"
+			kind("WindowedApp")
+			optimize "Full"
 			flags { "NoIncrementalLink", "LinkTimeOptimization" }
+	
+group "thirdparty"
+	project "glad"
+		location "%{prj.group}/%{prj.name}"
+		language "C"
+		kind("StaticLib")
+		warnings "off"
+		
+		includedirs{
+		"%{prj.location}/include/"
+		}
+		
+		srcDir = "%{prj.group}/{prj.name}/src/"
+		
+		files
+		{
+		srcDir .. "**.h",
+		srcDir .. "**.cpp"
+		}			
+		filter "configurations:Release"
+			optimize "Full"
+			flags { "NoIncrementalLink", "LinkTimeOptimization" }
+group ""
 			
