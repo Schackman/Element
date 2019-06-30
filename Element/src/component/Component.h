c0e4a8bf00ba122c6efeea65b9c6f9b8e99abff0
Stijn Schack <stijn.schack@gmail.com>
2019-06-30 14:47:27 +0200

    moved macro defenition

--- Element/src/component/Component.h
+++ Element/src/component/Component.h
@@ -5,15 +5,15 @@ namespace elm {namespace comp
 {
 	template <typename ComponentType>
 	struct Component;
+}}
 
 #define ASSERT_COMPONENT_TYPE(T) static_assert(std::is_base_of_v<elm::comp::Component<T>, T>);
-}}
 
 #define DEFINE_COMPONENT_TYPE(COMP)\
 class COMP;\
 template<> struct elm::comp::Component<COMP>\
 {\
-	inline constexpr static unsigned id = __COUNTER__;\
+	inline constexpr static unsigned id = __COUNTER__ + 1;\
 	inline constexpr static char name[] = #COMP;\
 };\
 class COMP final : public Component<COMP>
